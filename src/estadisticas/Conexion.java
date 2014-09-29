7d41f99853d539197fa49e4f919ed19272b0f414
Maria de los Angeles <aguilangeles@gmail.com>
2014-09-29 11:18:30 -0300

    creo conexion

--- src/estadisticas/Conexion.java
+++ src/estadisticas/Conexion.java
@@ -19,96 +19,65 @@ import java.util.logging.Logger;
  * @author aguilangeles@gmail.com
  */
 public class Conexion {
+
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private Connection conexion;
     private Statement statement;
     private ResultSet resultSet;
     private PreparedStatement preparedStatement;
     private String url, base, user, password;
+  private int filasAfectadas;
 
     public Conexion() {
     }
-    
-    public boolean isconnect() throws SQLException{
+
+    public boolean isconnect() throws SQLException {
         try {
             Class.forName(DRIVER);
             String urlext = "jdbc:mysql://" + url + "/" + base;
-            conexion =(Connection) DriverManager.getConnection(urlext, user, password);
-            
+            conexion = (Connection) DriverManager.getConnection(urlext, user, password);
+
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
-            return conexion != null;
+        return conexion != null;
     }
-    
-      public boolean isConexionClose() {
-    if (resultSet != null)
-      {
-      try
-        {
-        resultSet.close();
-        } catch (SQLException ex)
-        {
-            
-        }
-      }
-    if (statement != null)
-      {
-      try
-        {
-        statement.close();
-        } catch (SQLException ex)
-        {
 
-        }
-      }
-    if (conexion != null)
-      {
-      try
-        {
-        if (!conexion.isClosed())
-          {
-          try
-            {
-            conexion.close();
+    public boolean isConexionClose() {
 
-            return true;
-            } catch (SQLException ex)
-            {
-
-                return false;
+        try {
+            if (resultSet != null && statement != null && !conexion.isClosed()) {
+                resultSet.close();
+                statement.close();
+                conexion.close();
+                return true;
             }
-          }
-        } catch (SQLException ex)
-        {
-
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
-      }
-    return false;
-  }
-      
-        public void executeQuery(String sql) {
-    try
-      {
-      statement = (Statement) (java.sql.Statement) conexion.createStatement();
-      resultSet = statement.executeQuery(sql);
+        return false;
+    }
 
-      } catch (SQLException sqle)
-      {
+
+    public void executeQuery(String sql) {
+        try {
+            statement = (Statement) (java.sql.Statement) conexion.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+        } catch (SQLException sqle) {
 //envia mensajes si la consulta tuvo un error
-      do
-        {
-        System.out.println("SQL STATE: " + sqle.getSQLState());
-        System.out.println("ERROR CODE: " + sqle.getErrorCode());
-        System.out.println("MESSAGE: " + sqle.getMessage());
-        System.out.println();
-        sqle = sqle.getNextException();
-        } while (sqle != null);
-      }
-  }
+            do {
+                System.out.println("SQL STATE: " + sqle.getSQLState());
+                System.out.println("ERROR CODE: " + sqle.getErrorCode());
+                System.out.println("MESSAGE: " + sqle.getMessage());
+                System.out.println();
+                sqle = sqle.getNextException();
+            } while (sqle != null);
+        }
+    }
 
-  public void executeUpdate(String sql) throws SQLException {
-//    prepareStatement = conexion.prepareStatement(sql);
-//    filasAfectadas = prepareStatement.executeUpdate();
-  }
+    public void executeUpdate(String sql) throws SQLException {
+    preparedStatement = (PreparedStatement) conexion.prepareStatement(sql);
+    filasAfectadas = preparedStatement.executeUpdate();
+    }
 }
